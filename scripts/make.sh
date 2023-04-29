37756d4165ef71326a332db2e10ad2db381a77b4
Liam Stanley <me@liamstanley.io>
2023-04-29 15:56:47 -0400

    exit with error when no make target is found
    Signed-off-by: Liam Stanley <me@liamstanley.io>

--- scripts/make.sh
+++ scripts/make.sh
@@ -6,6 +6,10 @@ fi
 
 for i in "$@"; do
 	if grep -qE "^${i}:" Makefile; then
+		echo "found: 'make $i', running..."
 		make "$i" && exit 0 || exit 1
 	fi
 done
+
+echo "no make targets found matching: ${*}"
+exit 1
