963e23196e2b10d8f2670500c9f35c1c67010510
Liam Stanley <me@liamstanley.io>
2023-05-03 22:50:53 -0400

    add bucket owner controls
    Signed-off-by: Liam Stanley <me@liamstanley.io>

--- terraform/aws-resources/s3_backup.tf
+++ terraform/aws-resources/s3_backup.tf
@@ -12,9 +12,19 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "backup" {
   }
 }
 
+resource "aws_s3_bucket_ownership_controls" "backup" {
+  bucket = aws_s3_bucket.backup.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
 resource "aws_s3_bucket_acl" "backup" {
   bucket = aws_s3_bucket.backup.id
   acl    = "private" # public-read
+
+  depends_on = [aws_s3_bucket_ownership_controls.backup]
 }
 
 resource "aws_s3_bucket_public_access_block" "backup" {
