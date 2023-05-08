b056a2a9faff89160683d8e21da5750b75027444
Liam Stanley <me@liamstanley.io>
2023-05-07 23:16:00 -0400

    add additional permissions to backup user
    Signed-off-by: Liam Stanley <me@liamstanley.io>

--- terraform/aws-resources/iam_backup.tf
+++ terraform/aws-resources/iam_backup.tf
@@ -12,11 +12,22 @@ data "aws_iam_policy_document" "backup_s3" {
     resources = [aws_s3_bucket.backup.arn, "${aws_s3_bucket.backup.arn}/*"]
 
     actions = [
-      "s3:GetObjectAcl",
+      "s3:AbortMultipartUpload",
       "s3:DeleteObject",
+      "s3:DeleteObjectTagging",
+      "s3:DeleteObjectVersion",
+      "s3:DeleteObjectVersionTagging",
+      "s3:Describe*",
+      "s3:Get*",
+      "s3:List*",
+      "s3:PutBucketTagging",
+      "s3:PutBucketVersioning",
       "s3:PutObject",
-      "s3:GetObject",
       "s3:PutObjectAcl",
+      "s3:PutObjectRetention",
+      "s3:PutObjectTagging",
+      "s3:PutObjectVersionAcl",
+      "s3:PutObjectVersionTagging",
     ]
   }
 }
