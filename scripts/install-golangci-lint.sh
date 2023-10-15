9ec2ff9779f5fc4df873b413d3f21baadd00d0da
Liam Stanley <me@liamstanley.io>
2023-10-15 15:15:44 -0400

    add svu and golangci-lint to meta-updaters
    Signed-off-by: Liam Stanley <me@liamstanley.io>

---
+++ scripts/install-golangci-lint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -eu
+# shellcheck disable=SC2155
+
+set -o pipefail
+
+GCI_VERSION="1.53.0"
+
+echo "installing golangci-lint ${GCI_VERSION}"
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin "v${GCI_VERSION}"
+
+# manually install for now, until https://github.com/golangci/golangci-lint/pull/3835 appears in a release.
+# go install github.com/golangci/golangci-lint/cmd/golangci-lint@f08711794c0b3e4606eed0cadf1cc57c11737c01
