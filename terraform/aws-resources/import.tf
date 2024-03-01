b528cbc8bdbb396bdca67882f53e88f81984f655
Liam Stanley <me@liamstanley.io>
2024-03-01 00:35:39 -0500

    maybe?
    Signed-off-by: Liam Stanley <me@liamstanley.io>

---
+++ terraform/aws-resources/import.tf
@@ -0,0 +1,9 @@
+import {
+  to = aws_ce_anomaly_monitor.default
+  id = "arn:aws:ce::435713168200:anomalymonitor/673c1b46-1d5d-40b8-b6a2-3a9310eca967"
+}
+
+import {
+  to = aws_ce_anomaly_subscription.default
+  id = "arn:aws:ce::435713168200:anomalysubscription/85851b60-0443-4346-895a-7779aee142ba"
+}
