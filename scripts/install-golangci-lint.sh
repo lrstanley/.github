073ffe63f11e35e95b35623bed800259b90eebf2
Liam Stanley <liam@liam.sh>
2025-01-09 00:28:56 -0500

    test github-based renovate
    Signed-off-by: Liam Stanley <liam@liam.sh>

--- scripts/install-golangci-lint.sh
+++ scripts/install-golangci-lint.sh
@@ -8,6 +8,3 @@ GCI_VERSION="1.63.4"
 
 echo "installing golangci-lint ${GCI_VERSION}"
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin "v${GCI_VERSION}"
-
-# manually install for now, until https://github.com/golangci/golangci-lint/pull/3835 appears in a release.
-# go install github.com/golangci/golangci-lint/cmd/golangci-lint@f08711794c0b3e4606eed0cadf1cc57c11737c01
