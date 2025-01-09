1c3f06b50c1c4af95f4f75ea1894189093bcc54a
Liam Stanley <liam@liam.sh>
2025-01-09 00:13:33 -0500

    add bot credentials
    Signed-off-by: Liam Stanley <liam@liam.sh>

--- terraform/github-common-secrets/locals.tf
+++ terraform/github-common-secrets/locals.tf
@@ -2,9 +2,12 @@ locals {
   module_name = basename(abspath(path.module))
 
   secrets = {
-    USER_PAT      = var.secret-user-pat
-    SNYK_TOKEN    = var.secret-snyk-token
-    CODECOV_TOKEN = var.secret-codecov-token
+    USER_PAT            = var.secret-user-pat
+    SNYK_TOKEN          = var.secret-snyk-token
+    CODECOV_TOKEN       = var.secret-codecov-token
+    BOT_APP_ID          = var.secret-github-bot-app-id
+    BOT_INSTALLATION_ID = var.secret-github-bot-installation-id
+    BOT_PRIVATE_KEY     = var.secret-github-bot-private-key
   }
 }
 
@@ -20,5 +23,17 @@ variable "secret-snyk-token" {
 }
 
 variable "secret-codecov-token" {
+  sensitive = true
+}
+
+variable "secret-github-bot-app-id" {
+  sensitive = true
+}
+
+variable "secret-github-bot-installation-id" {
+  sensitive = true
+}
+
+variable "secret-github-bot-private-key" {
   sensitive = true
 }
