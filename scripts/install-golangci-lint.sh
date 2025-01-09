203a4054da2cccd658be029794c131333fa69aa0
Liam Stanley <liam@liam.sh>
2025-01-09 01:23:10 -0500

    test lowering version
    Signed-off-by: Liam Stanley <liam@liam.sh>

--- scripts/install-golangci-lint.sh
+++ scripts/install-golangci-lint.sh
@@ -4,7 +4,7 @@
 set -o pipefail
 
 # renovate: datasource=github-releases depName=golangci/golangci-lint
-GCI_VERSION="1.63.4"
+GCI_VERSION="1.63.3"
 
 echo "installing golangci-lint ${GCI_VERSION}"
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin "v${GCI_VERSION}"
